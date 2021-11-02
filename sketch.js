04e23705d66ba9775fae3a464cc5bf99cd468c55
LeeCH <79134646+leechh0948@users.noreply.github.com>
2021-11-02 15:18:21 +0900

    Add New Method : attractTo

--- sketch.js
+++ sketch.js
@@ -1,16 +1,22 @@
 let m;
+let m2;
 
 function setup() {
   createCanvas(400, 400);
   m = new Mover(50, 50, 10);
+  m2 = new Mover(300, 300, 50);
 }
 
 function draw() {
   background(220);
   m.update();
   m.show();
-  
+  m2.update();
+  m2.show();
+
   if (mouseIsPressed) {
-    //make attract to mouse position
+    let mousePos = createVector(mouseX, mouseY);
+    m.attractTo(mousePos);
+    m2.attractTo(mousePos);
   }
 }
