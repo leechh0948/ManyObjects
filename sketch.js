09f249e0cb8da582e65f93d4fa61f90594ff6145
LeeCH <79134646+leechh0948@users.noreply.github.com>
2021-11-02 14:13:49 +0900

    Edit p5js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,16 @@
+let m;
+
 function setup() {
   createCanvas(400, 400);
+  m = new Mover(50, 50, 10);
 }
 
 function draw() {
   background(220);
-  ellipse(mouseX, mouseY, 50);
+  m.update();
+  m.show();
+  
+  if (mouseIsPressed) {
+    //make attract to mouse position
+  }
 }
